c78e5163ef79b6b0012ea42c7ade002ba68880ea
Jingu Kang <jinguk0816@gmail.com>
2021-05-24 21:38:22 -0400

    validations for stores and employees table

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,10 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Store name please? "
+store_name = gets.chomp
+@new_store = Store.create(name: store_name)
+
+if @new_store.errors.any?
+	@new_store.errors.each {|error_type, error_message| puts "#{error_type} : #{error_message}"}
+end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,14 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, presence: true, length: { minimum: 3}
+  validates :annual_revenue, presence: true, numericality: {only_interger: true, :greater_than_or_equal_to => 0}
+
+  validate :must_have_mens_or_womens_apparel
+
+  def must_have_mens_or_womens_apparel
+    if !mens_apparel && !womens_apparel
+      errors.add(:mens_apparel, "Cannot be blank")
+      errors.add(:womens_apparel, "Cannot be blank")
+    end
+  end
 end
