e9f3ea5be19977d039cd87d5f166914e9fd4d7e3
Jingu Kang <jinguk0816@gmail.com>
2021-05-24 20:45:16 -0400

    print total and average revenue of stores table and also print number of stores that make more than M in a year

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,7 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts "Total revenue is #{Store.sum(:annual_revenue)}"
+puts "Average anual revenue for all stores is #{Store.average(:annual_revenue)}"
+
+puts "Number of stores that are generating more than $1M in annual sales are #{Store.where("annual_revenue <=?", 1000000).count}"
